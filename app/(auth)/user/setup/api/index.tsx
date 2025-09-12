8ccf22122f68b4df8eb55b01fdc7ac43da251485
Rahulghimire <ghimirerahul554@gmail.com>
2025-09-12 09:14:01 +0545

    chanehs

---
+++ app/(auth)/user/setup/api/index.tsx
@@ -0,0 +1,75 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+  user_email: string;
+  phone_number: string;
+}
+
+export interface UserList {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  data: User[];
+}
+
+export const fetchApi = async (url: string): Promise<UserList> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${url}`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+    },
+  });
+
+  if (!res.ok) throw new Error("Error fetching users");
+  return res.json();
+};
+
+export const createApi = async (url: string, data: Omit<User, "id">) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}${url}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) throw new Error("Error creating user");
+  return res.json();
+};
+
+
+export const updateApi = async (url: string, user: User) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}${url}${user.id}/`,
+     {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+    },
+    body: JSON.stringify(user),
+  });
+
+  if (!res.ok) throw new Error("Error updating user");
+  return res.json();
+};
+
+
+
+export const deleteApi = async (url: string) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}${url}`,
+     {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+    },
+  });
+
+  if (!res.ok) throw new Error("Error deleting user");
+  return res.json();
+};
