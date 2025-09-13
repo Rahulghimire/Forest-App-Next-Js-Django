1b25bb74c26a5a71bb67d68a306ee9fc95536d58
Rahu Ghimire <ghimirerahul554@gmail.com>
2025-09-13 17:54:36 +0545

    chanegs

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,20 @@
 import { Spin } from "antd";
+import React from "react";
 
 export default function Loading() {
+  const contentStyle: React.CSSProperties = {
+    padding: 50,
+    background: "rgba(0, 0, 0, 0.05)",
+    borderRadius: 4,
+    width: "200px",
+  };
+
+  const content = <div style={contentStyle} />;
+
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <Spin tip="Forest Management System" size="large">
-        Forest Management System
+        {content}
       </Spin>
     </div>
   );
